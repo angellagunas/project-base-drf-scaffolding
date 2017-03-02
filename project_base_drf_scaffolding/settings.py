e545fde7b0d317ddd5d2e22728c56c45fdca3c8a
Angel Lagunas <angel.david.lagunas@gmail.com>
2017-03-02 11:52:54 -0600

    Add polls in INSTALLED APPS

--- project_base_drf_scaffolding/settings.py
+++ project_base_drf_scaffolding/settings.py
@@ -39,6 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+LOCAL_APPS = [
+    'polls'
+]
+
+INSTALLED_APPS += LOCAL_APPS
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
