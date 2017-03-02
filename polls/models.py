ac912094a5935897798439a902f59e866aa36d10
Angel Lagunas <angel.david.lagunas@gmail.com>
2017-03-02 12:15:45 -0600

    Add drf_scaffolding on models

--- polls/models.py
+++ polls/models.py
@@ -4,10 +4,21 @@ from django.db import models
 
 
 class Poll(models.Model):
+    class Meta:
+        drf_config = {
+            'api': True
+        }
     title = models.CharField(max_length=100)
     body = models.CharField(max_length=100)
 
 
 class Question(models.Model):
+    class Meta:
+        drf_config = {
+            'api': True,
+            'fields': [
+                'title'
+            ]
+        }
     title = models.CharField(max_length=100)
     body = models.CharField(max_length=100)
